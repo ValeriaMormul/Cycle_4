6c0106c812e5c48bf55918524ef8480b73b4628b
Valeriia Mormul <152411242+ValeriaMormul@users.noreply.github.com>
2023-12-05 00:18:16 +0200

    Добавьте файлы проекта.

---
+++ Cycle_4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число для вычисления факториала: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        // Обчислення факторіалу числа
+        long factorial = 1;
+
+        for (int i = 1; i <= number; i++)
+        {
+            factorial *= i;
+        }
+
+        // Виведення результату
+        Console.WriteLine($"Факториал числа {number}: {factorial}");
+    }
+}
